954f4fb904d4dbecdc6fa8468bda36154295c84b
Andre <60677720+AndresDevvv@users.noreply.github.com>
2023-02-24 18:01:44 -0500

    Add files via upload

---
+++ adblocker.php
@@ -0,0 +1,138 @@
+<script type="text/javascript">
+	atOptions = {
+		'key' : 'b66ed82adec43788ed996f9058e1729f',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivecreativeformat.com/b66ed82adec43788ed996f9058e1729f/invoke.js"></scr' + 'ipt>');
+</script>
+    <script type="text/javascript">
+	atOptions = {
+		'key' : 'b66ed82adec43788ed996f9058e1729f',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivecreativeformat.com/b66ed82adec43788ed996f9058e1729f/invoke.js"></scr' + 'ipt>');
+</script>
+    <script type="text/javascript">
+	atOptions = {
+		'key' : 'b66ed82adec43788ed996f9058e1729f',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivecreativeformat.com/b66ed82adec43788ed996f9058e1729f/invoke.js"></scr' + 'ipt>');
+</script>
+    <script type="text/javascript">
+	atOptions = {
+		'key' : 'b66ed82adec43788ed996f9058e1729f',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivecreativeformat.com/b66ed82adec43788ed996f9058e1729f/invoke.js"></scr' + 'ipt>');
+</script>
+<script type="text/javascript">
+	atOptions = {
+		'key' : 'b66ed82adec43788ed996f9058e1729f',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivecreativeformat.com/b66ed82adec43788ed996f9058e1729f/invoke.js"></scr' + 'ipt>');
+</script><script type="text/javascript">
+	atOptions = {
+		'key' : 'b66ed82adec43788ed996f9058e1729f',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivecreativeformat.com/b66ed82adec43788ed996f9058e1729f/invoke.js"></scr' + 'ipt>');
+</script><script type="text/javascript">
+	atOptions = {
+		'key' : 'b66ed82adec43788ed996f9058e1729f',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivecreativeformat.com/b66ed82adec43788ed996f9058e1729f/invoke.js"></scr' + 'ipt>');
+</script>
+<center>Hello, Please Disable Your Adblocker. Ads Is The Only Way We Make Money And We Need Money To Keep This Site Up So Please Disable Your AdBlocker. <a href="https://oregoncharter.zendesk.com/hc/en-us/articles/6347902029207-How-can-I-disable-uBlock-for-a-specific-website-">If You Dont Know How Heres How</a>  (uBlock Origin)</center>
+<center><a href="index.php"><button>I disabled my adblocker let me in!</button></a></center>
+<script type="text/javascript">
+	atOptions = {
+		'key' : 'b66ed82adec43788ed996f9058e1729f',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivecreativeformat.com/b66ed82adec43788ed996f9058e1729f/invoke.js"></scr' + 'ipt>');
+</script>
+    <script type="text/javascript">
+	atOptions = {
+		'key' : 'b66ed82adec43788ed996f9058e1729f',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivecreativeformat.com/b66ed82adec43788ed996f9058e1729f/invoke.js"></scr' + 'ipt>');
+</script>
+    <script type="text/javascript">
+	atOptions = {
+		'key' : 'b66ed82adec43788ed996f9058e1729f',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivecreativeformat.com/b66ed82adec43788ed996f9058e1729f/invoke.js"></scr' + 'ipt>');
+</script>
+    <script type="text/javascript">
+	atOptions = {
+		'key' : 'b66ed82adec43788ed996f9058e1729f',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivecreativeformat.com/b66ed82adec43788ed996f9058e1729f/invoke.js"></scr' + 'ipt>');
+</script>
+<script type="text/javascript">
+	atOptions = {
+		'key' : 'b66ed82adec43788ed996f9058e1729f',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivecreativeformat.com/b66ed82adec43788ed996f9058e1729f/invoke.js"></scr' + 'ipt>');
+</script><script type="text/javascript">
+	atOptions = {
+		'key' : 'b66ed82adec43788ed996f9058e1729f',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivecreativeformat.com/b66ed82adec43788ed996f9058e1729f/invoke.js"></scr' + 'ipt>');
+</script><script type="text/javascript">
+	atOptions = {
+		'key' : 'b66ed82adec43788ed996f9058e1729f',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivecreativeformat.com/b66ed82adec43788ed996f9058e1729f/invoke.js"></scr' + 'ipt>');
+</script>
---
+++ index.php
@@ -0,0 +1,119 @@
+<html>
+  <head>
+    
+    <title>Basr</title>
+      <link href="/proton/style.css" rel="stylesheet" type="text/css" media="all">
+  </head>
+  <body>
+
+    
+    <center><a href="/proton/index.php"><button class="playminecraft">Proton</button></a></center>
+
+    <center><a href="/proton/underdev.php"><button class="onlinemc">Movies</button></a></center>
+    
+    <center><a href="/proton/paid.php"><button class="offlinemc">Proton Pro</button></a></center>
+<p></p>
+
+
+<script>
+async function detectAdBlock() {
+  let adBlockEnabled = false
+  const googleAdUrl = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js'
+  try {
+    await fetch(new Request(googleAdUrl)).catch(_ => adBlockEnabled = true)
+  } catch (e) {
+    if (e.message !== 'net::ERR_CONNECTION_REFUSED') {
+      adBlockEnabled = true
+    }
+  } finally {
+    if (adBlockEnabled) {
+      alert("Ad Blocker Detected!");
+      location.href = 'adblocker.php';
+    } else {
+      alert("No Ad Blocker Detected Thanks For Supporting Us :).");
+    
+    }
+  }
+}
+
+setTimeout(() => {
+  detectAdBlock();
+}, 2000);
+
+</script>
+
+
+
+
+
+
+
+
+    <script type="text/javascript">
+	atOptions = {
+		'key' : 'b66ed82adec43788ed996f9058e1729f',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivecreativeformat.com/b66ed82adec43788ed996f9058e1729f/invoke.js"></scr' + 'ipt>');
+</script>
+    <script type="text/javascript">
+	atOptions = {
+		'key' : 'b66ed82adec43788ed996f9058e1729f',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivecreativeformat.com/b66ed82adec43788ed996f9058e1729f/invoke.js"></scr' + 'ipt>');
+</script>
+    <script type="text/javascript">
+	atOptions = {
+		'key' : 'b66ed82adec43788ed996f9058e1729f',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivecreativeformat.com/b66ed82adec43788ed996f9058e1729f/invoke.js"></scr' + 'ipt>');
+</script>
+    <script type="text/javascript">
+	atOptions = {
+		'key' : 'b66ed82adec43788ed996f9058e1729f',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivecreativeformat.com/b66ed82adec43788ed996f9058e1729f/invoke.js"></scr' + 'ipt>');
+</script>
+<script type="text/javascript">
+	atOptions = {
+		'key' : 'b66ed82adec43788ed996f9058e1729f',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivecreativeformat.com/b66ed82adec43788ed996f9058e1729f/invoke.js"></scr' + 'ipt>');
+</script><script type="text/javascript">
+	atOptions = {
+		'key' : 'b66ed82adec43788ed996f9058e1729f',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivecreativeformat.com/b66ed82adec43788ed996f9058e1729f/invoke.js"></scr' + 'ipt>');
+</script><script type="text/javascript">
+	atOptions = {
+		'key' : 'b66ed82adec43788ed996f9058e1729f',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivecreativeformat.com/b66ed82adec43788ed996f9058e1729f/invoke.js"></scr' + 'ipt>');
+</script>
